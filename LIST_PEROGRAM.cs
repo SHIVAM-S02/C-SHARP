c222cf205a9d7378c88d07e66503f42616a58f1a
SHIVAM SHARMA <sharmashiva4423@gmail.com>
2023-06-21 13:24:35 +0530

    CODE

---
+++ LIST_PEROGRAM.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+
+namespace LIST_PEROGRAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Length Of the List:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            List<Student> list = new List<Student>(num);
+
+            for (int i = 0; i < num; i++)
+            {
+                Student numb = new Student();
+
+                Console.Write("Enter The Name:");
+                numb.name = Console.ReadLine();
+
+                Console.Write("Enter The id:");
+                numb.id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter The Age:");
+                numb.age = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter The hobby:");
+                numb.hobby = Console.ReadLine();
+
+                Console.WriteLine(" ");
+                Console.WriteLine("**********************");
+                Console.WriteLine(" ");
+
+                list.Add(numb);
+
+            }
+            foreach (Student number in list)
+            {
+                Console.WriteLine(number.name + number.id  + number.age  + number.hobby);
+                
+            }
+            Console.WriteLine();
+        }
+    }
+    public class Student
+    {
+
+        public string name;
+        public int id;
+        public int age;
+        public string hobby;
+
+        public void student_data()
+        {
+            Console.WriteLine();
+        }
+
+
+    }
+
+}
+
+
---
+++ PROGRAM TO FIND AREA OF RECTANGLE AND CIRCLE.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AREA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle rect = new rectangle();
+            rect.Area();
+            rect = new Circle();
+            rect.Area();
+
+        }
+    }
+    public class rectangle
+    {
+        public double length;
+        public double breath;
+        
+        public virtual void Area()
+        {
+            Console.Write("Enter The Length:");
+            length = int.Parse(Console.ReadLine());
+            Console.Write("Enter The Breath:");
+            breath = int.Parse(Console.ReadLine());
+
+            Console.Write("The Area of rectangle:");
+            Console.WriteLine(length * breath);
+
+            Console.WriteLine(" ");
+        }
+        
+
+    }
+
+    public class Circle : rectangle
+    {
+        public double radius;
+        public  override void Area()
+        {
+
+            Console.Write("Enter The Radius:");
+            radius = int.Parse(Console.ReadLine());
+
+            Console.Write("The Area of Circle is:");
+            Console.WriteLine(3.14 * radius * radius);
+        }
+
+    }
+}
+ 
